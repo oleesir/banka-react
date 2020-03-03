41df12f7dceff3b437fc5d39d5dceca2b8401320
oleesir <oleesir@gmail.com>
2020-03-03 11:02:45 +0100

    implement ft-signup-page-171476002 [Finishes #171476002]

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import Signup from'./pages/auth/Signup';
+import Login from './pages/auth/Login';
+import Profile from './pages/profile/Profile';
+import AuthState from '../../client/src/context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
+
+
+if(localStorage.token){
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Banka App</h1>
-    </div>
+    <AuthState>
+      <AlertState>
+      <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path='/signup' component={Signup}/>
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/' component={Profile}/>
+        </Switch>
+        </div>
+      </Router>
+      </AlertState> 
+    </AuthState>
+    
   );
 }
 
 export default App;
+
---
+++ client/src/pages/auth/Signup.jsx
@@ -0,0 +1,130 @@
+import React,{ useContext, useEffect } from 'react';
+import {useForm} from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import './Auth.css';
+import AuthContext from '../../context/auth/authContext';
+import AlertContext from '../../context/alert/alertContext';
+import Alert from '../../components/layouts/Alerts';
+
+
+
+const Signup = (props) => {
+  const authContext = useContext(AuthContext);
+  const alertContext = useContext(AlertContext);
+
+  const{ register, handleSubmit, errors, formState: { isValid } } = useForm({ mode: 'onChange' });
+  const { signUp, error, clearErrors,isAuthenticated } = authContext;
+  const { setAlert, removeAlert, alerts} = alertContext;
+
+  const onChange = () => {
+    clearErrors();
+    removeAlert(alerts[0]?.id);
+  }
+
+  const onSubmit = ({ firstName, lastName, email, password }) => {
+    if (error) {
+      setAlert(error, 'danger');
+    }
+    signUp({
+      firstName,
+      lastName,
+      email,
+      password
+    });
+  }
+
+  useEffect(() => {
+    if(isAuthenticated){
+      props.history.push('/');
+    }
+
+    if (error) {
+      setAlert(error, 'danger', 'form-error');
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
+  return (
+    <div className='container auth-container'>
+      <div className='main'>
+          <header className='brand' >
+                Bank of olive
+          </header>
+          <div className='auth-content'>
+          <h1 className='login-header'>Sign Up</h1>
+            <Alert/>
+            <div className='form-content'>
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <input type='text' 
+                  name='firstName'
+                  placeholder='Firstname'
+                  onChange={onChange}
+                  ref={register({
+                    required: 'First name is required',
+                    pattern: {
+                      value: /^[A-Za-z]+$/i,
+                      message: 'First name should contain only letters'
+                    } 
+                  })}/>
+                {errors.firstName && <small className="input-error">{errors.firstName.message}</small>}
+          
+
+                <input type='text' 
+                name='lastName'
+                 placeholder='Lastname'
+                 onChange={onChange}
+                 ref={register({
+                  required: 'Last name is required',
+                  pattern: {
+                    value: /^[A-Za-z]+$/i,
+                    message: 'Last name should contain only letters'
+                  } 
+                })}/>
+                {errors.lastName && <small className="input-error">{errors.lastName.message}</small>}
+                
+                <input type='email' 
+                name='email'
+                placeholder='Email'
+                onChange={onChange}
+                ref={register({
+                  required: 'Email is required',
+                  pattern: {
+                    value:/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                    message: 'Please enter a valid email address'
+                  } 
+                })}/>
+                {errors.email && <small className="input-error">{errors.email.message}</small>}
+
+                <input type='password' 
+                  name='password'
+                  placeholder='Password'
+                  onChange={onChange}
+                  ref={register({
+                    required: 'Password is required',
+                    minLength: {
+                      value: 6,
+                      message: 'Password should not be less than 6 characters'
+                    } 
+                    })}
+                  />
+                  <div>
+                  {errors.password && <small className="input-error">{errors.password.message}</small>}
+                  </div>
+               
+                  <input  type='submit' className='btn btn-md login-bottom' disabled={!isValid} value='Sign up'/>
+                <div className='switch'>
+                    <p> Already have an account? </p>
+                      <Link to='/login'>Login</Link>
+                </div>
+              </form>
+            </div>
+          </div>
+      </div>
+      <div className='footer'>
+      <p>Olive ©2020</p>
+      </div>
+    </div>
+  );
+}
+
+export default Signup;
